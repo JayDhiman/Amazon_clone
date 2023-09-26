7d9cfb25a8a0af5f26d2d465095b8c9f5662ad76
JayDhiman <jaydhiman01@gmail.com>
2023-09-27 01:05:44 +0530

    addded new files like cart.js and update the cart section

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@ products.forEach((product)=>{
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id ="${product.id}">
       Add to Cart
     </button>
   </div>
@@ -57,3 +57,36 @@ products.forEach((product)=>{
 // console.log(productsHTML);
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productId =button.dataset.productId;
+      let matchingitem;
+      
+      cart.forEach((item)=>{
+        if(productId === item.productId){
+          matchingitem = item;
+        }
+      })
+      if(matchingitem){
+        matchingitem.quantity += 1;
+
+      }else{
+        cart.push({
+          productId: productId,
+          quantity: 1
+        })
+      }
+       let cartQuantity = 0;
+       cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+       })
+
+
+       document.querySelector(".js-cart-quantity")
+       .innerHTML = cartQuantity
+
+    
+    })
+})
+
